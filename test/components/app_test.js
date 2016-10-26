ce9f8d5332203062fec30427d42616b04798447e
ck <thecurrencykid@gmail.com>
2016-10-26 16:20:00 -0400

    expect App to contain Comment form

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -5,15 +5,18 @@ import App from '../../src/components/app';
 //group together similar tests w describe
 // takes first arg of a string, second arg of a funciton
 describe('App', () => {
+  let component; 
+
+  beforeEach(()=>{
+    component = renderComponent(App); 
+  });
 
   //use it to test single attribute of a target
   it('shows the correct text', () => {
 
     //App component instance
-    const component= renderComponent(App); 
-
 
     // make assertion about target w expect
-    expect(component).to.contain('React simple starter'); 
+    expect(component.find('.comment-box')).to.exist; 
   }); 
 });
